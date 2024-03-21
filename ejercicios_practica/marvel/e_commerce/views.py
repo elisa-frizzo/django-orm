64c056625f4307c1bd35f2e0340e51d24afa9f49
Elisa Frizzo <146304418+elisa-frizzo@users.noreply.github.com>
2024-03-21 09:47:06 -0300

    Add files via upload

--- ejercicios_practica/marvel/e_commerce/views.py
+++ ejercicios_practica/marvel/e_commerce/views.py
@@ -10,6 +10,9 @@ def comic_list_api_view(request):
         # Seguir los pasos detallados en
         # el archivo de enunciado de tarea
         print("Endpoint: comic_list_api_view")
+        _queryset = Comic.objects.all()
+        _data = list(_queryset.values()) if _queryset.exists() else []
+        return JsonResponse(data=_data, safe=False, status=200)
     else:
         return JsonResponse(data={"message": "Método HTTP no permitido."}, status=405)
 
@@ -21,6 +24,9 @@ def comic_filter_stock_api_view(request):
         # Seguir los pasos detallados en
         # el archivo de enunciado de tarea
         print("Endpoint: comic_filter_stock_api_view")
+        _queryset = Comic.objects.filter(stock_qty=5)
+        _data = list(_queryset.values()) if _queryset.exists() else []
+        return JsonResponse(data=_data, safe=False, status=200)        
     else:
         return JsonResponse(data={"message": "Método HTTP no permitido."}, status=405)
 
@@ -32,6 +38,9 @@ def comic_filter_price_api_view(request):
         # Seguir los pasos detallados en
         # el archivo de enunciado de tarea
         print("Endpoint: comic_filter_price_api_view")
+        _queryset = Comic.objects.filter(price__gt=3)
+        _data = list(_queryset.values()) if _queryset.exists() else []
+        return JsonResponse(data=_data, safe=False, status=200)        
     else:
         return JsonResponse(data={"message": "Método HTTP no permitido."}, status=405)
 
@@ -43,5 +52,8 @@ def comic_list_order_api_view(request):
         # Seguir los pasos detallados en
         # el archivo de enunciado de tarea
         print("Endpoint: comic_list_order_api_view")
+        _queryset = Comic.objects.order_by('marvel_id')
+        _data = list(_queryset.values()) if _queryset.exists() else []
+        return JsonResponse(data=_data, safe=False, status=200)        
     else:
         return JsonResponse(data={"message": "Método HTTP no permitido."}, status=405)
